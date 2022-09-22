1a93ae08723b43efc219e2d86de054d6c880cdb3
Arsen <ardakbekovarsen2000@gmail.com>
2022-09-22 22:55:27 +0300

    new faile

---
+++ lib/dace_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class arsen extends StatefulWidget {
+  const arsen({super.key});
+
+  @override
+  State<arsen> createState() => _arsenState();
+}
+
+class _arsenState extends State<arsen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
